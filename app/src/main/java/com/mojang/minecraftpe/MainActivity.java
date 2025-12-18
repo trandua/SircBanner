4132bab2015bbdd10bd6130687dd626b0509c9dd
Trần Đua <trandua@gmail.com>
2025-12-18 10:58:26 +0700

    fix
    fix upgrade gradle version

--- app/src/main/java/com/mojang/minecraftpe/MainActivity.java
+++ app/src/main/java/com/mojang/minecraftpe/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.unity3d.sircbanner.databinding.ActivityMainBinding;
+import com.craftsman.go.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
